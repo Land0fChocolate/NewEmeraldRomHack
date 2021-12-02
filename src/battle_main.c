d5e2206c0322e7c586b8e0739928a06d1b96b902
Land0fChocolate <tebeckingham@gmail.com>
2021-12-02 22:12:11 +0000

    Implement Burmy form change

--- src/battle_main.c
+++ src/battle_main.c
@@ -4889,6 +4889,7 @@ static void HandleEndTurn_FinishBattle(void)
         {
             UndoMegaEvolution(i);
             UndoFormChange(i, B_SIDE_PLAYER, FALSE);
+            DoBurmyFormChange(i);
         }
         gBattleMainFunc = FreeResetData_ReturnToOvOrDoEvolutions;
         gCB2_AfterEvolution = BattleMainCB2;
