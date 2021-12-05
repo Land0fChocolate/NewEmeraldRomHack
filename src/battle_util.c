60c5665d71bd14be0e26a37fc8010abdf3a0b0dc
Land0fChocolate <tebeckingham@gmail.com>
2021-12-05 12:15:13 +0000

    Expand move target field to u16

--- src/battle_util.c
+++ src/battle_util.c
@@ -6986,7 +6986,7 @@ u32 SetRandomTarget(u32 battlerId)
     return target;
 }
 
-u8 GetMoveTarget(u16 move, u8 setTarget)
+u32 GetMoveTarget(u16 move, u8 setTarget)
 {
     u8 targetBattler = 0;
     u32 i, moveTarget, side;
