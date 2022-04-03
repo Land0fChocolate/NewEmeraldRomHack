eab062477e3355f486d31f3d7773efe2849c201c
Land0fChocolate <tebeckingham@gmail.com>
2022-04-03 23:26:28 +0100

    fixed bug, do not fuck with TRAINERS_COUNT from now on

--- include/constants/opponents.h
+++ include/constants/opponents.h
@@ -866,8 +866,8 @@
 // NOTE: Because each Trainer uses a flag to determine when they are defeated, there is only space for 9 additional trainers before trainer flag space overflows
 //       More space can be made by shifting flags around in constants/flags.h or changing how trainer flags are handled
 //       MAX_TRAINERS_COUNT can be increased but will take up additional saveblock space
-
-#define TRAINERS_COUNT                      861
-#define MAX_TRAINERS_COUNT                  862
+// NOTE: DO NOT CHANGE THESE VALUES
+#define TRAINERS_COUNT                      863
+#define MAX_TRAINERS_COUNT                  864
 
 #endif  // GUARD_CONSTANTS_OPPONENTS_H
