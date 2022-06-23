90168e2d21375a1fb28795f6202c2e3c098712f4
Land0fChocolate <tebeckingham@gmail.com>
2022-06-23 20:28:21 +0100

    Post Game Story: Birch Lab part2, Weather Institute implemented, some bug fixes

--- include/constants/vars.h
+++ include/constants/vars.h
@@ -198,7 +198,7 @@
 #define VAR_WEATHER_INSTITUTE_STATE          0x40B3
 #define VAR_SS_TIDAL_STATE                   0x40B4
 #define VAR_TRICK_HOUSE_ENTER_FROM_CORRIDOR  0x40B5
-#define VAR_TRICK_HOUSE_PUZZLE_7_STATE_2     0x40B6 // Leftover from RS, never set
+#define VAR_TRICK_HOUSE_PUZZLE_7_STATE_2     0x40B6 // unused. Leftover from RS, never set
 #define VAR_SLATEPORT_FAN_CLUB_STATE         0x40B7
 #define VAR_WEATHER_INSTITUTE_TRIGGER_STATE  0x40B8
 #define VAR_MT_PYRE_STATE                    0x40B9
