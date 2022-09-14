243c77a1fe0a741e0a9aa57c3bc883f818dc07d7
Land0fChocolate <tebeckingham@gmail.com>
2022-09-14 18:51:08 +0100

    add poltergeist animation

--- src/battle_anim.c
+++ src/battle_anim.c
@@ -1568,6 +1568,7 @@ const struct CompressedSpriteSheet gBattleAnimPicTable[] =
     {gBattleAnimSpriteGfx_OmegaSymbol, 0x0200, ANIM_TAG_OMEGA_SYMBOL},
     {gBattleAnimSpriteGfx_PrimalParticles, 0x0180, ANIM_TAG_PRIMAL_PARTICLES},
     {gBattleAnimSpriteGfx_Orbs, 0x0180, ANIM_TAG_STEEL_BEAM},
+    {gBattleAnimSpriteGfx_AuraSphere, 0x200, ANIM_TAG_POLTERGEIST},
 };
 
 const struct CompressedSpritePalette gBattleAnimPaletteTable[] =
@@ -2019,6 +2020,7 @@ const struct CompressedSpritePalette gBattleAnimPaletteTable[] =
     {gBattleAnimSpritePal_OmegaSymbol, ANIM_TAG_OMEGA_SYMBOL},
     {gBattleAnimSpritePal_PrimalParticles, ANIM_TAG_PRIMAL_PARTICLES},
     {gBattleAnimSpritePal_SteelBeam, ANIM_TAG_STEEL_BEAM},
+    {gBattleAnimSpritePal_Poltergeist, ANIM_TAG_POLTERGEIST},
 };
 
 const struct BattleAnimBackground gBattleAnimBackgroundTable[] =
