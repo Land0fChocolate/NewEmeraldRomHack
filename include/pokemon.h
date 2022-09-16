2d253f8b48cf2f8a47273929800cc5a6b4d0fd64
Land0fChocolate <tebeckingham@gmail.com>
2022-09-17 00:56:08 +0100

    Fixed PokemonSubstruct3 alignment

--- include/pokemon.h
+++ include/pokemon.h
@@ -51,7 +51,7 @@ struct PokemonSubstruct3
 
  /* 0x02 */ u16 metLevel:7;
  /* 0x02 */ u16 metGame:4;
- /* 0x03 */ u16 unused3_3:4;
+ /* 0x03 */ u16 unused1:4;
  /* 0x03 */ u16 otGender:1;
 
  /* 0x04 */ u32 hpIV:5;
@@ -61,6 +61,7 @@ struct PokemonSubstruct3
  /* 0x05 */ u32 spAttackIV:5;
  /* 0x06 */ u32 spDefenseIV:5;
  /* 0x07 */ u32 isEgg:1;
+///* 0x07 */ u32 unused2:1; // TODO: SAVE DATA UPDATE. Uncomment at the beginning of Playthrough #2
 
  /* 0x08 */ u32 coolRibbon:3;
  /* 0x08 */ u32 beautyRibbon:3;
