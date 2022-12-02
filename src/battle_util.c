4bd6bb23be0bad2e9f93e01400aeae0293b24223
Land0fChocolate <tebeckingham@gmail.com>
2022-12-02 00:30:10 +0000

    Fixed berserk not activating if mon falls to exactly half HP

--- src/battle_util.c
+++ src/battle_util.c
@@ -5448,7 +5448,7 @@ u8 AbilityBattleEffects(u8 caseID, u8 battler, u8 special, u16 moveArg)
                  && TARGET_TURN_DAMAGED
                  && IsBattlerAlive(battler)
                 // Had more than half of hp before, now has less
-                 && gBattleStruct->hpBefore[battler] > gBattleMons[battler].maxHP / 2
+                 && gBattleStruct->hpBefore[battler] >= gBattleMons[battler].maxHP / 2
                  && gBattleMons[battler].hp < gBattleMons[battler].maxHP / 2
                  && (gMultiHitCounter == 0 || gMultiHitCounter == 1)
                  && !(TestSheerForceFlag(gBattlerAttacker, gCurrentMove))
