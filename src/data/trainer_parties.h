5c44a1d050b62f28e5e513abf94f74d7cf371512
Land0fChocolate <tebeckingham@gmail.com>
2023-03-28 20:55:17 +0100

    fixed abilities in CalcFinalDmg

--- src/data/trainer_parties.h
+++ src/data/trainer_parties.h
@@ -12827,10 +12827,10 @@ static const struct TrainerMonNoItemDefaultMoves sParty_Red[] = {
 static const struct TrainerMonItemCustomMoves sParty_Leaf[] = {
     {
     .iv = 0,
-    .lvl = 50,
-    .species = SPECIES_CHINGLING,
+    .lvl = 7,
+    .species = SPECIES_POOCHYENA,
     .heldItem = ITEM_NONE,
-    .moves = {MOVE_ENTRAINMENT, MOVE_NONE, MOVE_NONE, MOVE_NONE}
+    .moves = {MOVE_SPLASH, MOVE_NONE, MOVE_NONE, MOVE_NONE}
     }
 };
 
